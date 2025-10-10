179a45afcfb6c0a963ee76af788403a93ffc89b6
nirmaladevieee555 <nirmaladevi.eee555.com>
2025-10-10 06:56:00 +0530

    dfghjk

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,23 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { MovieDetails, MoviesList, PageNotFound, Search } from "../pages";
 
-
 const AllRoutes = () => {
   return (
-    <Routes >
-        <Route path ="/" element={<MoviesList title="your guide to great movies" apiPath="movie/now_playing" />}/>
-        <Route path ="movies/popular" element={<MoviesList title="popular movies" apiPath="movie/popular" />}/>
-        <Route path ="movies/top" element={<MoviesList title="top rated  movies" apiPath="movie/top_rated" />}/>
-        <Route path ="movies/upcoming" element={<MoviesList title="upcoming movies" apiPath="movie/upcoming" />}/>
-        <Route path ="movie/:id" element={<MovieDetails /> } /> 
-        <Route path ="search" element={<Search apiPath="search/movie"/> } /> 
-        <Route path="*" element={<PageNotFound />} title="page not found" />
-
-        
-
-
+    <Routes>
+      <Route path="/" element={<MoviesList title="Your Guide to Great Movies" apiPath="movie/now_playing" />} />
+      <Route path="movies/popular" element={<MoviesList title="Popular Movies" apiPath="movie/popular" />} />
+      <Route path="movies/top" element={<MoviesList title="Top Rated Movies" apiPath="movie/top_rated" />} />
+      <Route path="movies/upcoming" element={<MoviesList title="Upcoming Movies" apiPath="movie/upcoming" />} />
+      <Route path="movie/:id" element={<MovieDetails />} />
+      <Route path="search" element={<Search apiPath="search/movie" />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default AllRoutes;
